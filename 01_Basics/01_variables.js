33f6c60a85a090140fcd251a267a6603a020786f
Sk Sofiquee Fiaz <sksofiqueefiaz2015@gmail.com>
2024-10-24 13:53:10 +0000

    added variable file

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 144553
+let accountEmail = "example@google.com"
+var accountPasswd = "12345"
+let accountState // empty variable Output: undefined
+
+accountCity = "burdwan" //prefer not to use 
+
+// accountId = 2  //NOT ALLOWED
+accountEmail = "sample@google.com"
+accountPasswd = "abcdef"
+accountCity = "Kolkata"
+
+console.log(accountId);
+/*
+prefer not to use 'var' keyword
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId, accountEmail, accountPasswd, accountCity, accountState])
+
+
