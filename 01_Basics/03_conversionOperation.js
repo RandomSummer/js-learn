32eb2d3cedaedcb1387711a7edc8c88a6cd778a7
Sk Sofiquee Fiaz <sksofiqueefiaz2015@gmail.com>
2024-10-24 16:37:10 +0000

    added conversion and operation

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,34 @@
+let score = "100abc"
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+
+/*
+Conversion of any datatype to number is
+String => NaN
+Boolean => true(1) and false(0)
+null => 0
+undefined => NaN
+*/
+
+// type of NaN is number as well 
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn);
+
+/*
+1 => true; 0 => false
+"" => false
+"RandomSummer" => true
+*/
+
+console.log(typeof(valueInNumber));
+
+console.log(valueInNumber); // Output: NaN
+
+
+
