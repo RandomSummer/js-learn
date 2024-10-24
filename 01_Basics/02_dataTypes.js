b935256917fe53334336547c02dcc1ed3b0f178c
Sk Sofiquee Fiaz <sksofiqueefiaz2015@gmail.com>
2024-10-24 14:17:31 +0000

    added dataTypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+// code readability must (important)
+
+let name = "RandomSummer"
+let age = 20
+let isBoy = true
+let state = null
+
+// Primitive DataTypes
+
+// number => 2 to power 53
+// bigInt
+// String => " "
+// boolean => true/false
+// null => standalone value // Output: object
+// undefined => value not assigned // Output: undefined
+// symbol => unique
+
+// Object
+
+console.log(typeof undefined);
+console.log(typeof null);
+
+
