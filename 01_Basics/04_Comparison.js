bd4eb1e9020ec2132d9e1bdef52cd786af86e0d7
Sk Sofiquee Fiaz <sksofiqueefiaz2015@gmail.com>
2024-10-25 12:14:33 +0000

    updated the comparison file

---
+++ 01_Basics/04_Comparison.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);  // avoid such comparison
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === 
+
+console.log("2" === 2); // Strict comparison it even check datatypes as well
