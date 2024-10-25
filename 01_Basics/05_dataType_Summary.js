56a47eca378f8ebf32e969553d4b7e1c1f4af94a
Sk Sofiquee Fiaz <sksofiqueefiaz2015@gmail.com>
2024-10-25 12:30:11 +0000

    added datatype summary file

---
+++ 01_Basics/05_dataType_Summary.js
@@ -0,0 +1,37 @@
+// Primitive 
+
+// 7 Types: String, number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3 // both are same (number datatype)
+
+const isLoggedIn = false
+const outsideTemp = null // object
+let userEmail; // undefined (by Default)
+
+const id = Symbol("123")
+const anotherId = Symbol("123")
+
+console.log(id === anotherId); // false
+
+const bigNumber = 3472987294247829
+
+
+// Reference (Non-Primitive)
+
+// Array, Objects, Functions
+
+const heroes = ["Ironman", "Thor", "Deadpool", "Wolverine"]
+
+let myObj = {
+    name: "RandomSummer",
+    age: 20,
+}
+
+const myFunciton = function(){
+    console.log("Hello World");
+    
+}
+
+console.log(typeof myFunciton)
+
